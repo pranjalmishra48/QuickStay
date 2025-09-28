242b653b1c36b4bd13b873efe7f233ce45741330
pranjalmishra48 <pranjalmishra1901@gmail.com>
2025-09-28 16:08:42 +0530

    Name change

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
   try {
     mongoose.connection.on('conected', ()=> console.log("Database Connected"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/Quickstay-Hotel Booking`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/QuickStay`)
   } catch (error) {
     console.log(error.message)
   }
