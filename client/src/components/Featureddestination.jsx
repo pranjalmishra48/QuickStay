56e610464a941585f8d626b8864cf7c801faa32c
pranjalmishra48 <pranjalmishra1901@gmail.com>
2025-09-30 20:31:04 +0530

    Project Completed

--- client/src/components/Featureddestination.jsx
+++ client/src/components/Featureddestination.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
 import Title from './Title'
 import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
 
 const Featureddestination = () => {
+  const {rooms, navigate} = useAppContext();
 
-  const navigate = useNavigate()
-  return (
-    <div className='flex flex-col item-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+  return rooms.length > 0 && (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
 
       <Title title='Featured Destination' subTitle='Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experience.' />
 
       <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-        {roomsDummyData.slice(0,4).map((room, index)=>(<HotelCard key={room._id} room={room} index={index}/>))}
+        {rooms.slice(0,4).map((room, index)=>(<HotelCard key={room._id} room={room} index={index}/>))}
       </div>
 
       <button onClick={()=>{navigate('/rooms'); scrollTo(0,0)}}
